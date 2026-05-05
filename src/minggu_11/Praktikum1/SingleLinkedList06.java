a07dcab3ea794e317c986991f5f9503255444be8
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-05-05 13:05:07 +0700

    1

---
+++ src/minggu_11/Praktikum1/SingleLinkedList06.java
@@ -0,0 +1,91 @@
+package minggu_11.Praktikum1;
+
+public class SingleLinkedList06 {
+    nodeMahasiswa06 head;
+    nodeMahasiswa06 tail;
+
+    //  method isEmpty
+    boolean isEmpty() {
+        return (head == null);
+    }
+
+    //  method print (traverse)
+    public void print() {
+        if (!isEmpty()) {
+            nodeMahasiswa06 tmp = head;
+            System.out.println("Isi Linked List:\t");
+            while (tmp != null) {
+                tmp.data.tampilInformasi();
+                tmp = tmp.next;
+            }
+            System.out.println("");
+        } else {
+            System.out.println("Linked list kosong");
+        }
+    }
+
+
+    //  method addFirst 
+    public void addFirst(mahasiswa06 input) {
+        nodeMahasiswa06 ndInput = new nodeMahasiswa06(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            ndInput.next = head;
+            head = ndInput;
+        }
+    }
+
+    
+    //  method addLast 
+    public void addLast(mahasiswa06 input) {
+        nodeMahasiswa06 ndInput = new nodeMahasiswa06(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            tail.next = ndInput;
+            tail = ndInput;
+        }
+    }
+
+    
+    //  method insertAfter 
+    public void insertAfter(String key, mahasiswa06 input) {
+        nodeMahasiswa06 ndInput = new nodeMahasiswa06(input, null);
+        nodeMahasiswa06 temp = head;
+        do {
+            if (temp.data.nama.equalsIgnoreCase(key)) {
+                ndInput.next = temp.next;
+                temp.next = ndInput;
+                if (ndInput.next == null) {
+                    tail = ndInput;
+                }
+                break;
+            }
+            temp = temp.next;
+        } while (temp != null);
+    }
+
+    
+    //  method insertAt (menyisipkan node pada indeks tertentu)
+    public void insertAt(int index, mahasiswa06 input) {
+        if (index < 0) {
+            System.out.println("indeks salah");
+        } else if (index == 0) {
+            addFirst(input);
+        } else {
+            nodeMahasiswa06 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = new nodeMahasiswa06(input, temp.next);
+            if (temp.next.next == null) {
+                tail = temp.next;
+            }
+        }
+    }
+}
+    
+
