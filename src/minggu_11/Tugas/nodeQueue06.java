679a35ae66e3c418b875b66e324b6ec69d02d731
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-05-05 14:46:56 +0700

    revisi

--- src/minggu_11/Tugas/nodeQueue06.java
+++ src/minggu_11/Tugas/nodeQueue06.java
@@ -1,11 +1,11 @@
 package minggu_11.Tugas;
 
 public class nodeQueue06 {
-     MahasiswaQueue06 data;
-     nodeQueue06 next;
- 
-    public nodeQueue06(MahasiswaQueue06 data) {
+    MahasiswaQueue06 data;
+    nodeQueue06 next;
+
+    public nodeQueue06(MahasiswaQueue06 data, nodeQueue06 next) {
         this.data = data;
-        this.next = null;
+        this.next = next;
     }
 }
