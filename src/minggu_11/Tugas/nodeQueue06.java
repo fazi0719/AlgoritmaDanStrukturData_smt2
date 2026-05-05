206acb9584112b281e7c18859457920bf6752cad
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-05-05 14:32:07 +0700

    tugas

---
+++ src/minggu_11/Tugas/nodeQueue06.java
@@ -0,0 +1,11 @@
+package minggu_11.Tugas;
+
+public class nodeQueue06 {
+     MahasiswaQueue06 data;
+     nodeQueue06 next;
+ 
+    public nodeQueue06(MahasiswaQueue06 data) {
+        this.data = data;
+        this.next = null;
+    }
+}
