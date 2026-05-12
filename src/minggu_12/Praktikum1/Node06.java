2fee844c716e5cfe1c483f19adf7aedab0091726
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-05-12 13:07:08 +0700

    12

---
+++ src/minggu_12/Praktikum1/Node06.java
@@ -0,0 +1,14 @@
+package minggu_12.Praktikum1;
+
+public class Node06 {
+    mahasiswa06 data;
+    Node06 prev;
+    Node06 next;
+
+ 
+    public Node06(mahasiswa06 data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
