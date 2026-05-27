cedf4c838a1c5a4e495e9ecadeb12c638798edc4
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-05-27 08:16:27 +0700

    .

---
+++ src/minggu_13/BinaryTreeMain06.java
@@ -0,0 +1,62 @@
+package minggu_13;
+
+public class BinaryTreeMain06 {
+   public static void main(String[] args) {
+        BinaryTree06 bst = new BinaryTree06();
+ 
+        bst.add(new mahasiswa06("244160121", "Ali", "A", 3.57));
+        bst.add(new mahasiswa06("244160221", "Badar", "B", 3.85));
+        bst.add(new mahasiswa06("244160185", "Candra", "C", 3.21));
+        bst.add(new mahasiswa06("244160220", "Dewi", "B", 3.54));
+ 
+        System.out.println("\nDaftar semua mahasiswa (in oder traversal):");
+        bst.traverseInOrder(bst.root);
+ 
+        System.out.println("\nPencarian data mahasiswa:");
+        System.out.print("Cari mahasiswa dengan ipk: 3.54 : ");
+        String hasilCari = bst.find(3.54) ? "Ditemukan" : "Tidak ditemukan";
+        System.out.println(hasilCari);
+ 
+        System.out.print("Cari mahasiswa dengan ipk: 3.22 : ");
+        hasilCari = bst.find(3.22) ? "Ditemukan" : "Tidak ditemukan";
+        System.out.println(hasilCari);
+ 
+        bst.add(new mahasiswa06("244160131", "Devi", "A", 3.72));
+        bst.add(new mahasiswa06("244160205", "Ehsan", "D", 3.37));
+        bst.add(new mahasiswa06("244160170", "Fizi", "B", 3.46));
+        System.out.println("\nDaftar semua mahasiswa setelah penambahan 3 mahasiswa:");
+        System.out.println("InOrder Traversal:");
+        bst.traverseInOrder(bst.root);
+        System.out.println("\nPreOrder Traversal:");
+        bst.traversePreOrder(bst.root);
+        System.out.println("\nPostOrder Traversal:");
+        bst.traversePostOrder(bst.root);
+
+        // TUGAS 1 
+        System.out.println("\n========== TUGAS 1: addRekursif() ==========");
+        BinaryTree06 bst2 = new BinaryTree06();
+        bst2.addRekursif(new mahasiswa06("244160121", "Ali", "A", 3.57));
+        bst2.addRekursif(new mahasiswa06("244160221", "Badar", "B", 3.85));
+        bst2.addRekursif(new mahasiswa06("244160185", "Candra", "C", 3.21));
+        bst2.addRekursif(new mahasiswa06("244160220", "Dewi", "B", 3.54));
+        bst2.addRekursif(new mahasiswa06("244160131", "Devi", "A", 3.72));
+        bst2.addRekursif(new mahasiswa06("244160205", "Ehsan", "D", 3.37));
+        bst2.addRekursif(new mahasiswa06("244160170", "Fizi", "B", 3.46));
+        System.out.println("InOrder traversal setelah addRekursif:");
+        bst2.traverseInOrder(bst2.root);
+
+         // TUGAS 2 
+        System.out.println("\n========== TUGAS 2: cariMinIPK() dan cariMaxIPK() ==========");
+        bst2.cariMinIPK();
+        bst2.cariMaxIPK();
+
+        //  TUGAS 3 
+        System.out.println("\n========== TUGAS 3: tampilMahasiswaIPKdiAtas(3.50) ==========");
+        bst2.tampilMahasiswaIPKdiAtas(3.50);
+    
+        System.out.println("\nPenghapusan data mahasiswa");
+        bst.delete(3.57);
+        System.out.println("\nDaftar semua mahasiswa setelah penghapusan 1 mahasiswa (in order traversal):");
+        bst.traverseInOrder(bst.root); 
+    }
+}
