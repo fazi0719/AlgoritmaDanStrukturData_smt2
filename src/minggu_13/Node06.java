3b80be0d0d2890c5ed894188293bfe23690784ab
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-05-27 08:15:23 +0700

    .

---
+++ src/minggu_13/Node06.java
@@ -0,0 +1,15 @@
+package minggu_13;
+
+public class Node06 {
+      mahasiswa06 mahasiswa;
+    Node06 left, right;
+ 
+    public Node06() {
+    }
+ 
+    public Node06(mahasiswa06 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
+ 
