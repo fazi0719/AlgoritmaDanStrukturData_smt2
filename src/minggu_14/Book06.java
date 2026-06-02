0f5a0b5f80797094ae5a48a87a2cbe79e54c118d
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-06-02 19:27:01 +0700

    .

--- src/minggu_14/Book06.java
+++ src/minggu_14/Book06.java
@@ -5,14 +5,13 @@ public class Book06 {
     public String title;
 
     public Book06(){
-
     }
 
     public Book06(String isbn, String title){
         this.isbn = isbn;
         this.title = title;
     }
-
+    // Method toString() untuk menampilkan informasi buku
     public String toString(){
         return "ISBN: " + this.isbn + " Title: " + this.title;
     }
