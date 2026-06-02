464ccce1dd2eb97c8ec3f143640e1e54d9058d0a
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-06-02 10:43:32 +0700

    .

---
+++ src/minggu_14/Customer06.java
@@ -0,0 +1,20 @@
+package minggu_14;
+
+public class Customer06 {
+    public int id;
+    public String name;
+
+    public Customer06() {
+
+    }
+
+    public Customer06(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString(){
+        return " ID: " + this.id + " Nama: " + this.name;
+    }
+}
+
